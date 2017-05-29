c2e786335496019fcbe5116dae6f42e06d102e57
Amit Malik <contact2amitmalik@gmail.com>
2017-05-29 15:03:23 +0530

    Update deploy-netapp-otc.sh

--- scripts/deploy-netapp-otc.sh
+++ scripts/deploy-netapp-otc.sh
@@ -6,26 +6,23 @@
 ## Argument 5 will be IP address of client VM 1 ##
 ## Argument 6 will be IP address of client VM 2 ##
 
-region=$1
-adminEmail=$2
-adminPassword=$3
-subscriptionId=$4
-azureTenantId=$5
-applicationId=$6
-applicationKey=$7
-vnetID=$8
-cidr="10.0.0.0/16"
-cidrnew=$9
-subnetID=$10
-nsgID=$11
-licenseType=$12
-instanceType=$13
-storageType=$14
-QuickstartNameTagName=15
-QuickstartNameTagValue=16
-QuickstartProviderTagName=17
-QuickstartProviderTagValue=18
-otcName=azureqsotc
+region=${1}
+adminEmail=${2}
+adminPassword=${3}
+subscriptionId=${4}
+azureTenantId=${5}
+applicationId=${6}
+applicationKey=${7}
+vnetID=${8}
+cidr=${9}
+subnetID=${10}
+nsgID=${11}
+licenseType=${12}
+instanceType=${13}
+storageType=${14}
+QuickstartNameTagValue=${15}
+QuickstartProviderTagValue=${16}
+otcName=${17}
 
 echo $region
 echo $adminEmail
@@ -41,9 +38,7 @@ echo $nsgID
 echo $licenseType
 echo $instanceType
 echo $storageType
-echo $QuickstartNameTagName
 echo $QuickstartNameTagValue
-echo $QuickstartProviderTagName
 echo $QuickstartProviderTagValue
 echo $otcName
 
@@ -71,21 +66,21 @@ sudo chmod +x /usr/bin/jq
 
 
 ## Setup Cloud Manager
-curl http://localhost/occm/api/occm/setup/init -X POST --header 'Content-Type:application/json' --header 'Referer:AzureQS' --data '{ "tenantRequest": { "name": "'$tenantName'", "description": "", "costCenter": "", "nssKeys": {} }, "proxyUrl": { "uri": "" }, "userRequest":{  "email": "'$adminEmail'","lastName": "user", "firstName":"admin","roleId": "'$roleID'","password": "'$adminPassword'", "ldap": "false", "azureCredentials": { "subscriptionId": "'$subscriptionId'", "tenantId": "'$azureTenantId'", "applicationId": "'$applicationId'", "applicationKey": "'$applicationKey'" }  }, "site": "'$siteName'", "company": "'$siteCompany'", "autoVsaCapacityManagement": "'$autoVsaCapacityManagement'",   "autoUpgrade": "'$autoUpgrade'" }}'
+curl http://localhost/occm/api/occm/setup/init -X POST --header 'Content-Type:application/json' --header 'Referer:AzureQS' --data '{ "tenantRequest": { "name": "'${tenantName}'", "description": "", "costCenter": "", "nssKeys": {} }, "proxyUrl": { "uri": "" }, "userRequest":{  "email": "'${adminEmail}'","lastName": "user", "firstName":"admin","roleId": "'${roleID}'","password": "'${adminPassword}'", "ldap": "false", "azureCredentials": { "subscriptionId": "'${subscriptionId}'", "tenantId": "'${azureTenantId}'", "applicationId": "'${applicationId}'", "applicationKey": "'${applicationKey}'" }  }, "site": "'${siteName}'", "company": "'${siteCompany}'", "autoVsaCapacityManagement": "'${autoVsaCapacityManagement}'",   "autoUpgrade": "'${autoUpgrade}'" }}'
 sleep 40
 
 until sudo wget http://localhost/occmui > /dev/null 2>&1; do sudo wget http://localhost > /dev/null 2>&1 ; done
 sleep 60
 
 ## Authenticate to Cloud Manager
-curl http://localhost/occm/api/auth/login --header 'Content-Type:application/json' --header 'Referer:AzureQS' --data '{"email":"'$adminEmail'","password":"'$adminPassword'"}' --cookie-jar cookies.txt
+curl http://localhost/occm/api/auth/login --header 'Content-Type:application/json' --header 'Referer:AzureQS' --data '{"email":"'${adminEmail}'","password":"'${adminPassword}'"}' --cookie-jar cookies.txt
 sleep 5
 
 ## Get the Tenant ID so we can create the ONTAP Cloud system in that Cloud Manager Tenant
 tenantId=`sudo curl http://localhost/occm/api/tenants -X GET --header 'Content-Type:application/json' --header 'Referer:AzureQS' --cookie cookies.txt | jq -r .[0].publicId`
 
 ## Create a ONTAP Cloud working env
-curl http://localhost/occm/api/azure/vsa/working-environments -X POST --cookie cookies.txt --header 'Content-Type:application/json' --header 'Referer:AzureQS' --data '{ "name": "'$otcName'", "svmPassword": "'$svmPassword'",  "vnetId": "'$vnetID'",   "cidr": [{"'$cidr'"}],  "description": "", "vsaMetadata": { "ontapVersion": "'$ontapVersion'", "licenseType": "'$licenseType'", "instanceType": "'$instanceType'" }, "volume": { "name": "'$sqlvolname'", "size": { "size": "'$sqlvolsize'", "unit": "'$unit'" }, "snapshotPolicyName": "'$snapshotPolicyName'", "exportPolicyInfo": { "policyType": "custom", "ips": ['$cidr'] }, "enableThinProvisioning": "'true'", "enableCompression": "false", "enableDeduplication": "false" }, "region": "'$region'", "tenantId": "'$tenantId'", "subnetId":"'$subnetID'", "dataEncryptionType":"NONE", "ontapEncryptionParameters": null, "securityGroupId":"'$nsgID'", "skipSnapshots": "false", "diskSize": { "size": "1","unit": "TB" }, "storageType": "'$storageType'", "azureTags": [ { "tagKey": "'$QuickstartNameTagName'", "tagKey": "'$QuickstartNameTagValue'"}, { "tagKey": "'$QuickstartProviderTagName'", "tagKey": "'$QuickstartProviderTagValue'"} ],"writingSpeedState": "NORMAL" }' > /tmp/createnetappotc.txt
+curl http://localhost/occm/api/azure/vsa/working-environments -X POST --cookie cookies.txt --header 'Content-Type:application/json' --header 'Referer:AzureQS' --data '{ "name": "'${otcName}'", "svmPassword": "'${svmPassword}'",  "vnetId": "'${vnetID}'",   "cidr": ["'${cidr}'"],  "description": "", "vsaMetadata": { "ontapVersion": "'${ontapVersion}'", "licenseType": "'${licenseType}'", "instanceType": "'${instanceType}'" }, "volume": { "name": "'${sqlvolname}'", "size": { "size": "'${sqlvolsize}'", "unit": "'${unit}'" }, "snapshotPolicyName": "'${snapshotPolicyName}'", "exportPolicyInfo": { "policyType": "custom", "ips": ["'${cidr}'"] }, "enableThinProvisioning": "'true'", "enableCompression": "false", "enableDeduplication": "false" }, "region": "'${region}'", "tenantId": "'${tenantId}'", "subnetId":"'${subnetID}'", "dataEncryptionType":"NONE", "ontapEncryptionParameters": null, "securityGroupId":"'${nsgID}'", "skipSnapshots": "false", "diskSize": { "size": "1","unit": "TB" }, "storageType": "'${storageType}'", "azureTags": [ { "tagKey": "'quickstartName'", "tagKey": "'${QuickstartNameTagValue}'"}, { "tagKey": "provider", "tagKey": "'${QuickstartProviderTagValue}'"} ],"writingSpeedState": "NORMAL" }' > /tmp/createnetappotc.txt
 OtcPublicId=`cat /tmp/createnetappotc.txt| jq -r .publicId`
 if [ ${OtcPublicId} = null ] ; then
   message=`cat /tmp/createnetappotc.txt| jq -r .message`
