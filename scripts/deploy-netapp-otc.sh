fd79cd5b514cd234649ea42d81b4111fac84fc5c
Amit Malik <contact2amitmalik@gmail.com>
2017-05-29 23:05:47 +0530

    Update deploy-netapp-otc.sh

--- scripts/deploy-netapp-otc.sh
+++ scripts/deploy-netapp-otc.sh
@@ -65,25 +65,32 @@ if [ ${OtcPublicId} = null ] ; then
 fi
 sleep 2
 
-## Check OTC Public ID
-waitForAction ${OtcPublicId} 60 1
 
 ## Getting the NetApp Ontap Cloud Cluster Properties
 
-
 curl 'http://localhost/occm/api/azure/vsa/working-environments/'${OtcPublicId}'?fields=status' -X GET --header 'Content-Type:application/json' --header 'Referer:AzureQS' --cookie cookies.txt > /tmp/envdetails.txt
 otcstatus=`cat /tmp/envdetails.txt| jq -r .[].status.status`
-while [ ${otcstatus} = null ] -o [ $stats -eq INITIALIZING ]
- do
-  message='Not Deployed Yet, Checking again in 60 seconds'
-  echo  ${message}
-  sleep 60
-  curl 'http://localhost/occm/api/azure/vsa/working-environments?/'${OtcPublicId}'fields=status' -X GET --header 'Content-Type:application/json' --header 'Referer:AzureQS' --cookie cookies.txt > /tmp/envdetails.txt
-  otcstatus=`cat /tmp/envdetails.txt| jq -r .[].status.status`
 
-done
+check_deploymentstatus()
+{
+curl 'http://localhost/occm/api/azure/vsa/working-environments/'${OtcPublicId}'?fields=status' -X GET --header 'Content-Type:application/json' --header 'Referer:AzureQS' --cookie cookies.txt > /tmp/envdetails.txt
+otcstatus=`cat /tmp/envdetails.txt| jq -r .[].status.status`
+}
 
-sleep 5
+if [ ${otcstatus} = null ]
+    then
+          message="Not Deployed Yet, Checking again in 60 seconds"
+          echo  ${message}
+          sleep 60
+          check_deploymentstatus
+ elif [ ${otcstatus} = INITIALIZING ]
+ then
+          message="Not Deployed Yet, Checking again in 60 seconds"
+          echo  ${message}
+          sleep 60
+          check_deploymentstatus
+fi
+sleep 20
 
 curl 'http://localhost/occm/api/azure/vsa/working-environments/'${OtcPublicId}'?fields=ontapClusterProperties' -X GET --header 'Content-Type:application/json' --header 'Referer:AzureQS' --cookie cookies.txt > /tmp/ontapClusterProperties.txt
 
