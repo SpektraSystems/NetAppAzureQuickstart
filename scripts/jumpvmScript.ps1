d5c73dfae05ed03100bca57b5662344b62d92abb
Jithin Varghese <jithinvarghese.varghese8@gmail.com>
2017-08-02 13:03:27 +0530

    Add files via upload

--- scripts/jumpvmScript.ps1
+++ scripts/jumpvmScript.ps1
@@ -1,14 +1,10 @@
-[CmdletBinding()]
-param(
-    [Parameter(Mandatory=$true)]
-    [String]$userName
-)
+
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
 
-#$DesktopPath = [Environment]::GetFolderPath("Desktop") 
-New-Item C:\Users\$userName\Desktop\sql-vm.rdp  -ItemType file
+$DesktopPath = [Environment]::GetFolderPath("Desktop") 
+New-Item $DesktopPath\sql-vm.rdp  -ItemType file
 $a="full address:s:10.0.2.4:3389
 prompt for credentials:i:1
 administrative session:i:1"
 
-Add-Content C:\Users\$userName\Desktop\sql-vm.rdp  $a
+Add-Content $DesktopPath\sql-vm.rdp  $a
