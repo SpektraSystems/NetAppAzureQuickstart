1b6827c889e868289dcb542a40f6e68090f4a822
Jithin Varghese <jithinvarghese.varghese8@gmail.com>
2017-08-03 18:29:22 +0530

    Update jumpvmScript.ps1

--- scripts/jumpvmScript.ps1
+++ scripts/jumpvmScript.ps1
@@ -1,10 +1,4 @@
 
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
 
-$DesktopPath = [Environment]::GetFolderPath("Desktop") 
-New-Item $DesktopPath\sql-vm.rdp  -ItemType file
-$a="full address:s:10.0.2.4:3389
-prompt for credentials:i:1
-administrative session:i:1"
-
-Add-Content $DesktopPath\sql-vm.rdp  $a
+start /wait msiexec.exe /i NetApp_PowerShell_Toolkit_4.3.0.msi /qn ADDLOCAL=F.PSTKDOT
