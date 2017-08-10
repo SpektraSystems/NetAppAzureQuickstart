2c7d43658b3955a098cdc291f3bab9ff7fe18881
Amit Malik <contact2amitmalik@gmail.com>
2017-08-11 00:06:04 +0530

    Add files via upload

---
+++ scripts/InstallNetAppPS.ps1
@@ -0,0 +1,5 @@
+﻿New-Item C:\NetApp -Type Directory
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/SpektraSystems/NetAppAzureQuickstart/master/scripts/NetApp_PowerShell_Toolkit_4.3.0.msi","C:\NetApp\NetApp_PowerShell_Toolkit_4.3.0.msi")
+# Start-Process msiexec.exe -Wait -ArgumentList '/I C:\NetApp\NetApp_PowerShell_Toolkit_4.3.0.msi /quiet'
+Invoke-Command -ScriptBlock { & cmd /c "msiexec.exe /i C:\NetApp\NetApp_PowerShell_Toolkit_4.3.0.msi" /qn ADDLOCAL=F.PSTKDOT}
